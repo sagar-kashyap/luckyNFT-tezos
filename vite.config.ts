aa43fff324cbe3927d0fc05c445626f860b1e44f
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2023-08-31 23:28:10 +0530

    axios build error solved

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: {},
+    
+    _global: ({})
   },
   build: {
     commonjsOptions: {
